4190615ec5f7eab5c21e4598ca891869be9f3724
jackrschumacher11 <84467112+jackrschumacher11@users.noreply.github.com>
2023-05-04 16:42:23 +0000

    Start Setup

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-class Main {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java Trail!");
-		System.out.println("This is a Java Version of the Popular Game, Oregon Trail.");
+class Main{
+	public static void main(String[] args){
+		JavaTrail game  = new JavaTrail();
+		game.startGame();
+		
 	}
-}
