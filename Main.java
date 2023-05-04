346e30d7460147f57bb1e4527dd59e6661574fae
jackrschumacher11 <84467112+jackrschumacher11@users.noreply.github.com>
2023-05-04 16:26:06 +0000

    Updated Opening

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Welcome to Java Trail!");
+		System.out.println("This is a Java Version of the Popular Game, Oregon Trail.");
 	}
 }
