f6fb75f8a00367c595b84bf4ad7b5622751de0f5
jackrschumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-05-09 16:39:29 +0000

    Add Starting Location

--- Intro.java
+++ Intro.java
@@ -1,5 +1,6 @@
 import java.util.*;
 public class Intro{
+	Scanner in = new Scanner(System.in);
 	public String Description(String version){
 		
 		System.out.println("Welcome to Java Trail");
@@ -7,8 +8,17 @@ public class Intro{
 		System.out.println("A Java- based Oregon Trail Game");
 		System.out.println("Developed By Jack Schumacher in 2023");
 		System.out.println("Version "+ version);
-	
+		Story();
 		return "";
+	}
+	public String Story(){
+		String startChoice;
+		System.out.println("You are about to embark on a journey westward.");
+		System.out.println("You can decide where you would like to begin your journey. Enter where you would like to start on the next line:");
+		startChoice = in.nextLine();
+		System.out.println("You have decided to start at:"+ startChoice);
+		return startChoice;
+		
 	}
 	
 	
