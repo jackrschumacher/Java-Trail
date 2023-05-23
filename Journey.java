59e744cda836ff01cd877159eb02321406db35ec
jackrschumacher <84467112+jackrschumacher@users.noreply.github.com>
2023-05-23 15:49:28 +0000

    Update For Loop and Start random event logic

--- Journey.java
+++ Journey.java
@@ -4,8 +4,27 @@ public class Journey{
 	public static String Travel(String startChoice, int moneyAmount){
 		System.out.println("You will now begin your journey from " + startChoice + " to Ventura, California");
 		int distanceToTravel = (int)(Math.random()* 10000);
+		int distanceRemaining = 0;
+		
 		System.out.println("You have " + distanceToTravel+ "miles to travel");
 		System.out.println("You will travel a different distance each day. \nAlong your journey, you will encounter many obstacles to your journey, such as rivers, natural disasters, animals and more. \nAddionally, you will encounter situations that might improve your journey, such as towns or trading posts, or other travelers. \nGood Luck!");
+		
+		for(int distance = 0; distance <= distanceToTravel; distance = distance + (int)(Math.random()*100)){
+			distanceRemaining = distanceToTravel - distance;
+			System.out.println("You have traveled:"+ distance+ "So far");
+			System.out.println("You have"+ distanceRemaining+ "miles left to travel");
+			int randomEvent = (int)(Math.random() * 10);
+			if(randomEvent == 1){
+				
+				System.out.println("Testing");
+			}
+			
+			
+			
+			
+		}
 		return "";
 	}
+	
+	
 }
